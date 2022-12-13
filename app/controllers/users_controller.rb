640d2e94f4a01105b91cf783e24f178f25c4c161
benaiah-ke <benaiahpraise@gmail.com>
2022-12-13 12:00:44 +0300

    Add user signup function - create method in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+    private
+
+    def user_params
+        params.permit(:name, :phone, :email, :location)
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json: {errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
 end
