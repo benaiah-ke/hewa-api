6ef474c8317135c349b0a9b2b8dc47ed7e3cebb4
benaiah-ke <benaiahpraise@gmail.com>
2022-12-13 12:19:29 +0300

    add database reset code to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Deleting initial seeds..."
+
+User.destroy_all
+CarbonProject.destroy_all
+Offsetter.destroy_all
+Emmitter.destroy_all
+
+puts "Resetting primary key sequence..."
+
+ActiveRecord::Base.connection.tables.each do |t|
+    ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = '#{t}'")
+end
+
+puts "Database reset complete!"
